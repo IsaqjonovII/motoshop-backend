4e2b7e56262c7d3d35a19e7d59809f15b5037565
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-01-18 14:28:17 +0500

    changed host

--- index.js
+++ index.js
@@ -31,13 +31,16 @@ fastify.register(authRoutes, { prefix: "/api/v0/auth" });
 fastify.register(adRoutes, { prefix: "/api/v0/ad" });
 (() => {
   try {
-    fastify.listen({ port: process.env.PORT || 8000 }, function (err, address) {
-      if (err) {
-        fastify.log.error(err);
-        process.exit(1);
+    fastify.listen(
+      { port: process.env.PORT || 8000, host: "0.0.0.0" },
+      function (err, address) {
+        if (err) {
+          fastify.log.error(err);
+          process.exit(1);
+        }
+        fastify.log.info(`Server is now listening on ${address}`);
       }
-      fastify.log.info(`Server is now listening on ${address}`);
-    });
+    );
   } catch (error) {
     fastify.log.error(error);
   }
