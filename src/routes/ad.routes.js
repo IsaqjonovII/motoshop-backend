08dd14941036a7f93954a876f0845774d9ae0004
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-03-13 21:27:45 +0500

    secure url added to images

--- src/routes/ad.routes.js
+++ src/routes/ad.routes.js
@@ -8,7 +8,7 @@ const {
   updateAdView,
   getSimilarAdsByType,
   toggleLikeAd,
-  getSearchedAds
+  getSearchedAds,
 } = require("../controllers/ad.controller");
 
 async function routes(fastify, _) {
@@ -24,28 +24,3 @@ async function routes(fastify, _) {
   fastify.get("/search", getSearchedAds);
 }
 module.exports = routes;
-
-
-
-// fastify.get("/delete-all", deleteAllImages);
-// const { v2 } = require("cloudinary");
-// async function deleteAllImages(_, reply) {
-//   try {
-    // const res = await v2.api.delete_resources_by_prefix("motocycles");
-//     const publicIds = [];
-//     const res = await v2.api.resources();
-//     await res.resources.forEach((img) => {
-//       publicIds.push(img.public_id);
-//     });
-//     const folders = await v2.api.resources({
-//       type: "upload",
-//     });
-//     await folders.resources.forEach((img) => {
-//       publicIds.push(img.public_id);
-//     });
-//     const deleted = await v2.api.delete_resources(publicIds);
-//     return reply.send(deleted);
-//   } catch (error) {
-//     return reply.send(error);
-//   }
-// }
